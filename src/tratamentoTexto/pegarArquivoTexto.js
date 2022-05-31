8b94cc0ed7157163f234a449ae5c7338c0ae582d
Jeff S.A <jeff_zb@msn.com>
2022-05-31 17:47:05 -0300

    alteracao para async pegarArquivoTexto

--- src/tratamentoTexto/pegarArquivoTexto.js
+++ src/tratamentoTexto/pegarArquivoTexto.js
@@ -2,13 +2,10 @@ import chalk from 'chalk';
 import fs from 'fs';
 import { lancarError } from '../error/lancarError.js';
 
-export function pegarArquivoTexto(caminhoArquivo) {
-    fs.readFile(caminhoArquivo, 'utf-8', (erro, texto) => {
-        if( erro )
-        {
-            lancarError(erro);
-        }
-        
-        console.log(chalk.blueBright(texto));
-    });
+export function pegarArquivoTexto(caminhoArquivo) 
+{
+    fs.promises
+        .readFile(caminhoArquivo, 'utf-8')
+            .then( (texto) => console.log(chalk.blueBright(texto)) )
+            .catch( (erro) => lancarError(erro) );
 }
